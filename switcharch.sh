a61987e8fe7bdfc20ee798f2b500c5f014d6419d
Huidae Cho <grass4u@gmail.com>
2019-12-18 03:57:08 -0500

    Add -q flag to switcharch.sh

--- switcharch.sh
+++ switcharch.sh
@@ -6,6 +6,7 @@
 #	switcharch.sh		# for compiling native binaries
 #	switcharch.sh mxe	# for cross-compiling x86_64-w64-mingw32
 #				# binaries using MXE
+#	switcharch.sh -q	# query the current architecture
 
 set -e
 . ~/.grassbuildrc
@@ -39,7 +40,23 @@ restore() {
 	done
 }
 
+MAKE=include/Make/Platform.make
+
+if [ -f $MAKE ]; then
+	CUR_ARCH=`sed -n '/^ARCH[ \t]*=/{s/^.*=[ \]*//; p}' $MAKE`
+else
+	CUR_ARCH=
+fi
+
 case "$1" in
+"-q")
+	if [ -z $CUR_ARCH ]; then
+		echo "Current architecture undefined"
+		exit 1
+	fi
+	echo "$CUR_ARCH: Current architecture"
+	exit
+	;;
 ""|native)
 	ARCH=`sh ./config.guess`
 	;;
@@ -54,27 +71,20 @@ mxe)
 esac
 
 ARCH_MAKE=include/Make/Platform.make.$ARCH
-MAKE=include/Make/Platform.make
 
-if [ -f $MAKE ]; then
-	CUR_ARCH=`sed -n '/^ARCH[ \t]*=/{s/^.*=[ \]*//; p}' $MAKE`
-	if [ "$ARCH" = "$CUR_ARCH" ]; then
-		if [ -f $ARCH_MAKE ]; then
-			if diff $ARCH_MAKE $MAKE > /dev/null; then
-				echo "$ARCH: Architecture already up to date"
-			else
-				restore
-				echo "$ARCH: Architecture restored"
-			fi
+if [ "$ARCH" = "$CUR_ARCH" ]; then
+	if [ -f $ARCH_MAKE ]; then
+		if diff $ARCH_MAKE $MAKE > /dev/null; then
+			echo "$ARCH: Architecture already up to date"
 		else
-			backup
-			echo "$ARCH: Architecture backed up"
+			restore
+			echo "$ARCH: Architecture restored"
 		fi
-		exit
+	else
+		backup
+		echo "$ARCH: Architecture backed up"
 	fi
-fi
-
-if [ -f $ARCH_MAKE ]; then
+elif [ -f $ARCH_MAKE ]; then
 	restore
 	echo "$ARCH: Architecture restored"
 else
