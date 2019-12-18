74aaf3da865108793da4b93478b3b977b0fe23a1
Huidae Cho <grass4u@gmail.com>
2019-12-17 21:08:20 -0500

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+(
+cd ~/usr/grass/grass
+git checkout master
+git fetch --all
+git merge upstream/master
+git checkout hcho
+git merge master
+../myconfigure.sh
+../mymake.sh clean default
+../myconfigure.sh mxe
+../mymake.sh clean default
+../copydocs.sh
+../mkfontcap.sh G:/grass
+../switcharch.sh
+
+cd ~/usr/local/src/gdal-grass-3.0.2
+../myconfigure-gdal-grass.sh
+make clean
+make
+make install
+
+cd ~/usr/grass/grass-addons
+git fetch --all
+git merge upstream/master
+cd grass7
+../../myaddons.sh clean default
+) > ~/usr/grass/update.log 2>&1
