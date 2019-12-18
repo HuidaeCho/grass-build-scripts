8063157f3430c09e6d347452f3148ba1ee577973
Huidae Cho <grass4u@gmail.com>
2019-12-17 20:30:09 -0500

    Add fontcap copydocs.sh

---
+++ copydocs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This script copies document files from the native build to non-native builds.
+
+set -e
+
+BUILD_ARCH=`sh ./config.guess`
+BUILD_DIST=dist.$BUILD_ARCH
+if [ ! -e $BUILD_DIST ]; then
+	echo "$BUILD_ARCH: Build the native architecture first"
+	exit 1
+fi
+
+for dist in dist.*; do
+	test $dist = $BUILD_DIST && continue
+	for i in \
+		docs \
+		gui/wxpython/xml \
+	; do
+		test -e $BUILD_DIST/$i || continue
+		rm -rf $dist/$i
+		cp -a $BUILD_DIST/$i $dist/$i
+	done
+done
