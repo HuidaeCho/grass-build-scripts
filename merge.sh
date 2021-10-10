366f2a88d8870d3a40ddae4f7fa71b1206b23fbb
Huidae Cho <grass4u@gmail.com>
2021-10-10 14:17:30 -0400

    master => main

--- merge.sh
+++ merge.sh
@@ -14,14 +14,14 @@ fi
 branches=`git branch -a --format='%(refname:short)'`
 
 git fetch --all
-git checkout master
-# if upstream/master exists, assume it's OSGeo's master branch
-if echo "$branches" | grep -q '^upstream/master$'; then
-	# merge OSGeo's master
-	git merge upstream/master
+git checkout main
+# if upstream/main exists, assume it's OSGeo's main branch
+if echo "$branches" | grep -q '^upstream/main$'; then
+	# merge OSGeo's main
+	git merge upstream/main
 else
-	# merge origin/master (either OSGeo's or HuidaeCho's master)
-	git merge origin/master
+	# merge origin/main (either OSGeo's or HuidaeCho's main)
+	git merge origin/main
 fi
 # if origin/hcho exists, assume it's HuidaeCho's hcho branch
 if echo "$branches" | grep -q '^origin/hcho$'; then
@@ -29,8 +29,8 @@ if echo "$branches" | grep -q '^origin/hcho$'; then
 	git checkout hcho
 	# merge origin/hcho
 	git merge origin/hcho
-	# merge master already merged with upstream/master or origin/master
-	git merge master
+	# merge main already merged with upstream/main or origin/main
+	git merge main
 fi
 # if upstream/grass7 exists, assume it's OSGeo's grass-addons repo
 if echo "$branches" | grep -q '^upstream/grass7'; then
