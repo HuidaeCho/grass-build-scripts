9794cb9bc495551875863c445182fd2ef35f2204
Huidae Cho <grass4u@gmail.com>
2022-12-04 15:04:11 -0700

    grass.py version

--- mkbats.sh
+++ mkbats.sh
@@ -7,10 +7,11 @@ set -e
 cd $GRASS_SRC
 
 arch=x86_64-w64-mingw32
+version=`sed -n '/^INST_DIR[ \t]*=/{s/^.*grass//; p}' include/Make/Platform.make`
 grass_build_scripts=$(dirname $(realpath $0))
 
-rm -f dist.$arch/grass.tmp
-cp -a bin.$arch/grass.py dist.$arch/etc
+rm -f bin.$arch/grass dist.$arch/grass.tmp
+[ -f bin.$arch/grass.py ] && mv bin.$arch/grass.py dist.$arch/etc/grass$version.py
 unix2dos -n $grass_build_scripts/grass.bat bin.$arch/grass.bat
 unix2dos -n $grass_build_scripts/sh.bat dist.$arch/etc/sh.bat
 
